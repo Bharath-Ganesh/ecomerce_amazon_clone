d62e8b6f134faa629b179961bcb876fba47c508c
bganesh2 <bharath110996@gmail.com>
2025-05-30 13:17:52 -0500

    Expose API endpoints for Inngest webhooks to trigger user sync functions

---
+++ api/inngest/route.js
@@ -0,0 +1,17 @@
+import { serve } from "inngest/next";
+/*
+  The `@` symbol in imports is a path alias (set in jsconfig.json/tsconfig.json) that points to a base directory, like src/. This makes imports shorter and easier to manage.
+*/
+import { inngest } from "@/config/inngest";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        /* your functions will be passed here later! */
+        syncUserCreation,
+        syncUserDeletion,
+        syncUserUpdation
+    ],
+});
+
