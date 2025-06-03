d0ad6f55fa200a205c02d182c2c98ae372752552
bganesh2 <bharath110996@gmail.com>
2025-06-02 23:05:45 -0500

    Remove obosolete footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
-        <div className="w-4/5">
+        {/* <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -14,7 +14,7 @@ const Footer = () => {
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </p>
-        </div>
+        </div> */}
 
         <div className="w-1/2 flex items-center justify-start md:justify-center">
           <div>
@@ -48,8 +48,8 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+1-217-979-4568</p>
+              <p>bganesh2@illinois.edu</p>
             </div>
           </div>
         </div>
