bb1e2f9541aef27e587321d9965ff8e59ed59477
bganesh2 <bharath110996@gmail.com>
2025-06-03 01:05:04 -0500

    User updated

--- models/User.js
+++ models/User.js
@@ -5,14 +5,16 @@ import mongoose from "mongoose";
 // This file defines the User schema and model for MongoDB using Mongoose.
 // It is used to store and manage user data synced from Clerk authentication events.
 
+// -----------------------------
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    imageUrl: { type: String, required: true },
     cartItems: { type: Object, default: {} }
 }, { minimize: false })
 
 const User = mongoose.models.user || mongoose.model('user', userSchema)
 
 export default User
+
